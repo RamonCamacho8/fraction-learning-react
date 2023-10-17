545a3423e841673f74bed525ba22e05138f1fe86
Ramon <118371477+RamonCamacho8@users.noreply.github.com>
2023-10-17 14:49:57 -0700

    First Commit

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import '../styles/App.css'
+export default function Board(){
+
+    return(
+        <div className="Board">
+            <h1 className='Title'>Board</h1>
+        </div>
+    );
+
+
+}
