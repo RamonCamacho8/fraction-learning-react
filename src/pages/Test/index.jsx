484e31e3c6dfabafdeea79319b5199ae2fb5616a
Ramon Camacho <118371477+RamonCamacho8@users.noreply.github.com>
2023-11-18 00:25:11 -0700

    Agregados servicios para los endpoints
    Además se agregó la configuración de colores

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,10 +1,23 @@
-import { testFunction, test_UploadJson } from "../../services/audio";
-
+import { test_get_text, test_post_text, test_post_json, test_get_json } from "../../services/test";
+import "./style.css";
+import { useState } from "react";
 const Test = () => {
+
+  const [text, setText] = useState("");
+  const [key, setKey] = useState("");
+  const [value, setValue] = useState("");
+
   return (
-    <div>
-      <button onClick={testFunction} >Test GET JSON</button>
-      <button onClick={test_UploadJson} >Test POST JSON</button>
+    <div className="audio-container">
+      <button onClick={test_get_text} >GET TEXT</button>
+      <button onClick={test_post_text} >POST TEXT</button>
+      <div>
+        <h2>JSON</h2>
+        <input type="text" placeholder="key" value={key} onChange={(e) => setKey(e.target.value)} />
+        <input type="text" placeholder="value" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={test_get_json} >GET JSON</button>
+        <button onClick={() =>test_post_json({[key]:value})} >POST JSON</button>
+      </div>
     </div>
   );
 };
