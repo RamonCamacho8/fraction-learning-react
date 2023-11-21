69953ac0596c9782eee68d7e95cc7badd7bc9872
Ramon Camacho <118371477+RamonCamacho8@users.noreply.github.com>
2023-11-21 14:51:41 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import './style.css';
 import Board from './pages/Board';
 import Home from './pages/Home';
 import Test from './pages/Test';
+
+import { PersonalityContext } from './Context/PersonalityContext';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,31 +17,35 @@ function App() {
   const [pNeuroticismo, setNeuroticismo] = useState(true);
   const [userName, setUserName] = useState('');
   const [lang, setLang] = useState('es');
-  
-  
+
+
+  const [aperture, setAperture] = useState(false);
+  const [neuroticism, setNeuroticism] = useState(false);
+
 
   return (
-      
-      <Routes>
-        <Route path="/" element={
-        <Home 
-          setApertura={setApertura} 
-          setNeuroticismo={setNeuroticismo} 
+
+    <Routes>
+      <PersonalityContext.Provider value = {{aperture: [aperture, setAperture], neuroticism: [neuroticism, setNeuroticism]}} /> 
+      <Route path="/" element={
+        <Home
+          setApertura={setApertura}
+          setNeuroticismo={setNeuroticismo}
           setUserName={setUserName}
-          language ={lang}
+          language={lang}
           setLanguageState={setLang}
 
         />} />
-        <Route path="/board" element={
-        <Board pApertura= {pApertura} pNeuroticismo={pNeuroticismo} userName = {userName}  language = {lang}
+      <Route path="/board" element={
+        <Board pApertura={pApertura} pNeuroticismo={pNeuroticismo} userName={userName} language={lang}
         />} />
-        <Route path="/test" element={
+      <Route path="/test" element={
         <Test />
-      } 
-        />
-        
-      </Routes>
-    
+      }
+      />
+
+    </Routes>
+
   );
 }
 
