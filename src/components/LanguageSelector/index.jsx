c1256a72fff0fbe7364cf2388c485ae4284e1cc4
Ramon Camacho <118371477+RamonCamacho8@users.noreply.github.com>
2023-11-22 17:22:27 -0700

    WIP

--- src/components/LanguageSelector/index.jsx
+++ src/components/LanguageSelector/index.jsx
@@ -1,5 +1,6 @@
 import Dropdown from 'react-bootstrap/Dropdown';
-import {getLanguageData, getLanguageList, setCurrentLanguage, getLanguage,  getHomeObject, getCurrentLanguage } from '../../services/Language.js';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import {getLanguageData, getLanguageList } from '../../services/Language.js';
 import { useLanguage } from '../../Context/LanguageContext.js';
 import './style.css';
 
@@ -8,19 +9,21 @@ import './style.css';
 
 function LanguageSelector() {
 
-  const {language,languageData, setLanguage, setLanguageData} = useLanguage();
+  const {language, languageData, setLanguage, setLanguageData} = useLanguage();
 
   const handleChangeLanguage = (event) => {
-    setLanguage(event.target.text);
-    console.log(language)
+
+    setLanguage(event.target.innerText);
     setLanguageData(getLanguageData(language));
+    console.log(language);
+
   }
   
 
   const availableLanguages = getLanguageList().map((lang, index) => {
     return (
       
-      <Dropdown.Item key={index} onClick={handleChangeLanguage}>{lang}</Dropdown.Item>
+      <Dropdown.Item as="button" key={lang} onClick={handleChangeLanguage} >{lang}</Dropdown.Item>
     
       );
   });
@@ -28,15 +31,10 @@ function LanguageSelector() {
   
 
   return (
-    <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        {language}
-      </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        {availableLanguages}
-      </Dropdown.Menu>
-    </Dropdown>
+  <DropdownButton id='dropdown-language-selector' title={`${language}`} >
+    {availableLanguages}
+  </DropdownButton>
   );
 }
 
