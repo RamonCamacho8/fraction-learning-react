324c27ef8bd922a7e14b5afa606e583ebfb7623f
Ramon Camacho <118371477+RamonCamacho8@users.noreply.github.com>
2024-01-26 10:51:05 -0700

    Add FirestoreTest and update firebase.js and Test page

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,52 +1,29 @@
-import { test_get_text, test_post_text, test_post_json, test_get_json } from "../../services/test";
-import { upload_audio } from "../../services/Audio";
 import "./style.css";
-import { useContext, useState } from "react";
-import AudioRecorder from "../../components/AudioRecorder";
-import { getPersonality } from "../../services/Personality";
+import { testAddDoc, testGetDocs } from "../../FirestoreTest";
+import { useEffect } from "react";
 
 const Test = () => {
 
-  const [text, setText] = useState("");
-  const [key, setKey] = useState("");
-  const [value, setValue] = useState("");
-  const [audioFile, setAudioFile] = useState(null);
+  /*useEffect(() => {
+    testGetDocs().then((response) => {
+      console.log(response);
+    });
+  }, []);*/
 
+  const handleTestAdd = () => {
+    const response = testAddDoc();
+    console.log(response);
+  };
 
-
-  const handleFileChange = (event) => {
-    setAudioFile(event.target.files[0]);
+  const handleTestGet = () => {
+    const response = testGetDocs();
+    console.log(response);
   };
 
   return (
-    <div className="audio-container">
-      
-      <div>
-        <h2>TEXT</h2>
-        <input type="text" placeholder="text" value={text} onChange={(e) => setText(e.target.value)} />
-        <button onClick={test_get_text} >GET TEXT</button>
-        <button onClick={() => test_post_text(text) } >POST TEXT</button>
-      </div>
-
-      <div>
-        <h2>JSON</h2>
-        <input type="text" placeholder="key" value={key} onChange={(e) => setKey(e.target.value)} />
-        <input type="text" placeholder="value" value={value} onChange={(e) => setValue(e.target.value)} />
-        <button onClick={getPersonality} >GET JSON</button>
-        <button onClick={() =>test_post_json({[key]:value})} >POST JSON</button>
-      </div>
-
-      <div>
-        <h2>AUDIO</h2>
-        <input type="file" accept="audio/*" onChange={handleFileChange} />
-        <button onClick={() => upload_audio(audioFile)} >POST AUDIO</button>
-      </div>
-
-      <div>
-        <h2>RECORDER</h2>
-        <AudioRecorder />
-      </div>
-
+    <div className="container">
+      <button onClick={handleTestAdd}>Add Docs</button>
+      <button onClick={handleTestGet}>Get Docs</button>
     </div>
   );
 };
